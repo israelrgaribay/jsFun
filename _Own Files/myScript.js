2450e9975fa8ae9684a544646c190f5ae5ac2ba0
Israel Garibay <35042273+israelrgaribay@users.noreply.github.com>
2019-11-09 19:29:23 -0800

    Add files via upload

---
+++ _Own Files/myScript.js
@@ -0,0 +1,12 @@
+/*var firstName = "Icarus";
+console.log(firstName);
+
+var lastName = "Garitech";
+var age = 25;
+var fullAge = true;
+console.log(fullAge);
+*/
+var name = "Icarus"
+var age = 25
+
+console.log(name + " is " + age + " years old."  )
